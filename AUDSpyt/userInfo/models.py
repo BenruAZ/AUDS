4473d9ce548d0a831f401e36d280b219152e737a
Unknown <rubengarciaabad@gmail.com>
2019-04-02 16:10:35 +0200

    Añadido Actvity y User Activity

--- AUDSpyt/userInfo/models.py
+++ AUDSpyt/userInfo/models.py
@@ -20,5 +20,21 @@ class UserInfo(models.Model):
 
 
 class User(models.Model):
-    userInfo = models.ForeignKey(UserInfo, on_delete=models.CASCADE),
+    userInfo = models.ForeignKey(UserInfo, on_delete=models.CASCADE)
     cosasSeguridad = models.CharField(max_length=250)
+
+class Activity(models.Model):
+    titulo = models.CharField(max_length=250)
+    descripcionCorta =  models.CharField(max_length=250)
+    descripcionLarga =  models.CharField(max_length=250)
+    icono =  models.ImageField(upload_to = 'IconoActividad/', default = 'pic_folder/None/no-img.jpg')
+    fechaFinal = models.CharField(max_length=250)
+    #def RESUMEN USER ACTIVITY
+
+
+
+class UserActivity(models.Model):
+    userInfo = models.ForeignKey(UserInfo, on_delete=models.CASCADE)
+    activity = models.ForeignKey(Activity, on_delete=models.CASCADE)
+    estado =  models.CharField(max_length=250) #Votacion(A favor - En contrA
+
